8215b0910c614ec72d835987737abfe4ee59abab
wojciechlasak <wojciech.lasak@outlook.com>
2021-04-22 12:46:56 +0200

    separated styled-component to new file

--- src/components/issue/Issue.js
+++ src/components/issue/Issue.js
@@ -2,35 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { getDate } from '../../utils/getDate';
 
-const Container = styled.div`
-  display: flex;
-  padding: 2em 0;
-  border-bottom: 1px solid ${props => props.theme.colors.grayLight};
-  transition: box-shadow 1s;
-
-  &:hover {
-    box-shadow: 20px 20px 20px rgba(0, 0, 0, 0.17);
-  }
-`;
-
-const Cell = styled.div`
-  padding: 0 1rem;
-  width: ${props => props.width || '18%'};
-  color: ${props => props.color || props.theme.colors.gray};
-  text-align: center;
-`;
-
-const TextCell = styled(Cell)`
-  overflow: hidden;
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-`;
-
-const EditCell = styled(Cell)`
-  cursor: pointer;
-  font-size: ${props => props.theme.fontSizes.medium};
-`;
+import { Container, TextCell, EditCell } from './StyledIssue';
 
 const Issue = ({ issue, onClickIssue }) => {
   return (
