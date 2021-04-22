1b0991156aa9b7c79d96777677972d07323a7dad
wojciechlasak <wojciech.lasak@outlook.com>
2021-04-22 21:39:39 +0200

    generated filterbox in loop

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect, useCallback } from 'react';
 import FilterBox from './FilterBox';
 import { IssuesContext } from '../../providers/IssuesProvider';
 import { getStatusColor } from '../../utils/getStatusColor';
@@ -7,10 +7,40 @@ import { Heading, Container } from './StyledHeader';
 const Header = () => {
   const issuesContext = useContext(IssuesContext);
   const [active, setActive] = useState('all');
+  const [filters, setFilters] = useState([]);
 
-  const getIssueNumber = status => {
-    return issuesContext.issues.filter(issue => issue.status === status).length;
-  };
+  const getIssueNumber = useCallback(
+    status => {
+      return issuesContext.issues.filter(issue => issue.status === status)
+        .length;
+    },
+    [issuesContext]
+  );
+
+  useEffect(() => {
+    setFilters([
+      {
+        name: 'all',
+        bgColor: '#283D7B',
+        number: issuesContext.issues.length,
+      },
+      {
+        name: 'open',
+        bgColor: getStatusColor('open'),
+        number: getIssueNumber('open'),
+      },
+      {
+        name: 'pending',
+        bgColor: getStatusColor('pending'),
+        number: getIssueNumber('pending'),
+      },
+      {
+        name: 'closed',
+        bgColor: getStatusColor('closed'),
+        number: getIssueNumber('closed'),
+      },
+    ]);
+  }, [issuesContext, getIssueNumber]);
 
   const handleChangeActive = title => {
     setActive(title);
@@ -21,34 +51,16 @@ const Header = () => {
     <React.Fragment>
       <Heading>Issues Tracker</Heading>
       <Container>
-        <FilterBox
-          title="all"
-          number={issuesContext.issues.length}
-          bgColor={'#283D7B'}
-          active={active}
-          changeActive={handleChangeActive}
-        />
-        <FilterBox
-          title="open"
-          number={getIssueNumber('open')}
-          bgColor={getStatusColor('open')}
-          active={active}
-          changeActive={handleChangeActive}
-        />
-        <FilterBox
-          title="pending"
-          number={getIssueNumber('pending')}
-          bgColor={getStatusColor('pending')}
-          active={active}
-          changeActive={handleChangeActive}
-        />
-        <FilterBox
-          title="closed"
-          number={getIssueNumber('closed')}
-          bgColor={getStatusColor('closed')}
-          active={active}
-          changeActive={handleChangeActive}
-        />
+        {filters.map(filter => (
+          <FilterBox
+            key={filter.name}
+            title={filter.name}
+            number={filter.number}
+            bgColor={filter.bgColor}
+            active={active}
+            changeActive={handleChangeActive}
+          />
+        ))}
       </Container>
     </React.Fragment>
   );
