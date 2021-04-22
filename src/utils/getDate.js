62051d8feb7dfc8efba92e3dac096a1f6bf9a9a5
wojciechlasak <wojciech.lasak@outlook.com>
2021-04-22 13:56:41 +0200

    added minutes and hours ago

--- src/utils/getDate.js
+++ src/utils/getDate.js
@@ -13,10 +13,31 @@ const monthNames = [
   'December',
 ];
 
-export function getDate(event_date, event_timezone_offset = 2) {
-  let d = new Date(event_date);
-  let timeCorrection = d.getTimezoneOffset() - event_timezone_offset;
+function sameDay(d1, d2) {
+  return (
+    d1.getFullYear() === d2.getFullYear() &&
+    d1.getMonth() === d2.getMonth() &&
+    d1.getDate() === d2.getDate()
+  );
+}
+
+function correctTime(d, eventTimezoneOffset) {
+  let timeCorrection = d.getTimezoneOffset() - eventTimezoneOffset;
   d.setMinutes(d.getMinutes() + timeCorrection);
+}
+
+export function getDate(eventDate, eventTimezoneOffset = 2) {
+  let d = new Date(eventDate);
+  let now = new Date();
+  correctTime(d, eventTimezoneOffset);
+  correctTime(now, eventTimezoneOffset);
+
+  if (sameDay(d, now)) {
+    let offsetDate = new Date(now - d);
+    let hours = offsetDate.getHours();
+    let minutes = offsetDate.getMinutes();
+    return hours === 0 ? `${hours} h ago` : `${minutes} min ago`;
+  }
 
   return `${monthNames[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
 }
